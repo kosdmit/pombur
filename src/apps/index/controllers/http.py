6cad6e67cfefdd0c75a1df5968eecb2d87a63dbf
kosdmit <kosdmit@hotmail.com>
2025-04-16 13:20:37 +0300

    Added index html template and assets

--- src/apps/index/controllers/http.py
+++ src/apps/index/controllers/http.py
@@ -1,10 +1,30 @@
 from litestar import Controller, MediaType, get
 from litestar.response import Template
+from pydantic import BaseModel
+
+
+class HeadPageMetaContext(BaseModel):
+    page_title: str
+    page_description: str
+    page_keywords: str
+
+
+class IndexPageContext(HeadPageMetaContext):
+    """Index page context.
+
+    Contains the data that is passed to the template as context.
+    """
 
 
 class IndexHTTPController(Controller):
     path = "/"
 
-    @get(media_type=MediaType.HTML)
+    @get(media_type=MediaType.HTML, name="index")
     async def get(self) -> Template:
-        return Template("index/index.html.jinja")
+        context = IndexPageContext(
+            page_title="Home",
+            page_description="Index page description",
+            page_keywords="Index page keywords",
+        )
+
+        return Template("index/index.html.jinja", context=context.model_dump())
